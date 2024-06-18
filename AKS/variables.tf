862f2fe11968e960fdc63b89eab1566dbe084dda
bannarisoftwares <75526665+bannarisoftwares@users.noreply.github.com>
2024-06-19 00:25:45 +0530

    Update variables.tf

--- AKS/variables.tf
+++ AKS/variables.tf
@@ -30,18 +30,3 @@ variable "username" {
 }
 */
 
-variable "client_id" {
-  type = string
-}
-
-variable "client_secret" {
-  type = string
-}
-
-variable "subscription_id" {
-  type = string
-}
-
-variable "tenant_id" {
-  type = string
-}
